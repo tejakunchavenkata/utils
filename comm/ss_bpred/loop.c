abdbdffb0d03f71d1f832a981b8a559a3fa477fa
krishna.vijay4444@gmail.com <krishna.vijay4444@gmail.com@ea77c94f-7c71-55d1-0cdb-b8471d5b3f0c>
2012-09-15 02:30:32 +0000

    ss bpred

---
+++ comm/ss_bpred/loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+        int i = 6;
+        int x, y, z;
+        while (i--) {
+                x = i+5;
+                y = i+i;
+                z = i+2;
+        }
+        x++;
+        y++;
+        z++;
+	return (0);
+}

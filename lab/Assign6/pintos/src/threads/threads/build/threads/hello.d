caf7e644caeb1b10afca0ab62728986da05f5e17
krishna.vijay4444@gmail.com <krishna.vijay4444@gmail.com@ea77c94f-7c71-55d1-0cdb-b8471d5b3f0c>
2012-10-27 08:45:25 +0000

    Assign6 started

---
+++ lab/Assign6/pintos/src/threads/threads/build/threads/hello.d
@@ -0,0 +1,3 @@
+threads/hello.o: ../../threads/hello.c ../../lib/stdio.h \
+ ../../lib/debug.h ../../lib/stdarg.h ../../lib/stdbool.h \
+ ../../lib/stddef.h ../../lib/stdint.h ../../lib/kernel/stdio.h

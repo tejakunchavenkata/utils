4f9abdf920c2e0be6be40c1019e6417d36a8c1de
krishna.vijay4444@gmail.com <krishna.vijay4444@gmail.com@ea77c94f-7c71-55d1-0cdb-b8471d5b3f0c>
2012-10-27 08:55:27 +0000

    deleting main copy

--- lab/pintos/src/threads/build/devices/timer.d
+++
@@ -1,6 +0,0 @@
-devices/timer.o: ../../devices/timer.c ../../devices/timer.h \
- ../../lib/round.h ../../lib/kernel/list.h ../../lib/stdbool.h \
- ../../lib/stddef.h ../../lib/stdint.h ../../lib/debug.h \
- ../../lib/inttypes.h ../../lib/stdio.h ../../lib/stdarg.h \
- ../../lib/kernel/stdio.h ../../devices/pit.h ../../threads/interrupt.h \
- ../../threads/synch.h ../../threads/thread.h
--- lab/pintos/src/threads/hello.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void hello_func () {
-  printf ("************************** Hello Programmer **************************\n");
-}

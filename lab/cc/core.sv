fa17b0f59a8f3985ada3690c86cf27e6b33428ff
krishna.vijay4444@gmail.com <krishna.vijay4444@gmail.com@ea77c94f-7c71-55d1-0cdb-b8471d5b3f0c>
2012-12-01 12:08:51 +0000

    update

--- lab/cc/core.sv
+++ lab/cc/core.sv
@@ -3,26 +3,27 @@ class packet;
         rand byte data;
         rand bit write;
 
-        function show (); begin
+        function show ();
                 if (write) begin
                         $display ("0x%08X Write 0x%08X", addr, data);
                 end else begin
                         $display ("0x%08X Read", addr);
                 end
-        end
+        endfunction
 endclass
 
 program core (input clk, input rst, core_cache_if.core_p cache_if);
         packet dp = new ();
 
-        forever begin
-                @ (posedge clk);
+        initial begin
+                forever begin
+                        @ (posedge clk);
 
-                if (rst) continue;
+                        if (rst) continue;
 
-                dp.randomize ();
-                dp.show ();
+                        dp.randomize ();
+                        dp.show ();
+                end
         end
-begin
 
 endprogram
